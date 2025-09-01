eec61c9bb0bf0f66ab5304acf8ee1355de6547c3
avishekp18 <avishekpradhan24@gmail.com>
2025-09-02 00:30:32 +0530

    firs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.cjs
 const { defineConfig } = require("vite");
 const react = require("@vitejs/plugin-react");
 
 module.exports = defineConfig({
   plugins: [react()],
-  base: "./skyTrack_Web", // ✅ important for Netlify
+  base: "/skyTrack_Web/", // ✅ must start and end with "/"
 });
