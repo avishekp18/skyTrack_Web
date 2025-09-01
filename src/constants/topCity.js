1fa68fd29f01539ca62fc005e41cd9e9f8f86656
avishekp18 <avishekpradhan24@gmail.com>
2025-09-02 01:35:40 +0530

    first

--- src/constants/topCity.js
+++ src/constants/topCity.js
@@ -5,7 +5,7 @@ export const topCities = [
   // Major Indian cities
   { id: 2, title: "Mumbai", lat: 19.076, lon: 42.8777 },
   // { id: 3, title: "Delhi", lat: 28.7041, lon: 77.1025 },
-  { id: 4, title: "Bengaluru", lat: 12.9716, lon: 77.5946 },
+  // { id: 4, title: "Bengaluru", lat: 12.9716, lon: 77.5946 },
   { id: 7, title: "Hyderabad", lat: 17.385, lon: 78.4867 },
 
   // Northern regions
